068a894e59485d936afbcab0bcba462436caa42c
saiprasad <saiprasadhegde192@gmail.com>
2025-04-25 16:08:25 +0530

    created a blog page and added first post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExercisesComponent } from './exercises/exercises.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
 
-export const routes: Routes = [ { path: '', component: HomeComponent, pathMatch: 'full' },{ path: 'exercises', component: ExercisesComponent, pathMatch: 'full' }];
+export const routes: Routes = [ { path: '', component: HomeComponent, pathMatch: 'full' },{ path: 'exercises', component: ExercisesComponent, pathMatch: 'full' },{ path: 'blogs', component: BlogPageComponent, pathMatch: 'full' }];
